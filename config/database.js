5f7739dba283369d1e3699697d0e0dd321cd781c
Dean <dmwspace@gmail.com>
2024-02-29 14:54:30 -0800

    deployment code

--- config/database.js
+++ config/database.js
@@ -1,11 +1,25 @@
-const mongoose = require('mongoose');
-
 // replace your database connection string here
-mongoose.connect(process.env.DATABASE_URL);
+// THE PURPOSE OF THIS FILE
+// is to establish a connection between our express server
+// and our database on mongodb atlas! 
 
-const db = mongoose.connection;
+// mongoose is the module that allows us to create that connection!
+const mongoose = require("mongoose");
 
-// database connection event
-db.on('connected', function () {
-  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
-});
+// export the function that creates a database connection
+module.exports = {
+  connectDB,
+};
+
+async function connectDB() {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+  } catch (err) {
+    console.log("err");
+    console.log(err, ' connecting to mongodb')
+    process.exit(1);
+  }
+}
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
+const MongoStore = require('connect-mongo');
+
 const indexRoutes = require('./routes/index');
 const transactionRoutes = require('./routes/transactions');
 const failureRoutes = require('./routes/failure');
@@ -36,6 +38,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // mount the session middleware
 app.use(session({
+  store: MongoStore.create({
+    mongoUrl: process.env.DATABASE_URL
+  }),
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true
